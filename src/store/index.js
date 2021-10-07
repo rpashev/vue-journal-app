63d4933b39eb0466ac90225c9f9e32e611a2ae12
rpashev <rossen1991@gmail.com>
2021-10-07 18:40:54 +0300

    worked on vue x user storage + first version of axios hook

--- src/store/index.js
+++ src/store/index.js
@@ -2,20 +2,54 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    isLoggedIn: true,
-    user: {
-      userName: "Rosko",
-    },
+    isLoggedIn: true, //temp
+    token: null,
+    userName: "Rosko", //temp
+    email: null,
+    userId: null,
+    firstName: null,
   },
   getters: {
     isLoggedIn(state) {
-      return state.isLoggedIn;
+      return state.isLoggedIn; //temp
     },
     userName(state) {
-      return state.user.userName;
+      return state.userName; //temp
+    },
+    token(state) {
+      return state.token;
+    },
+    email(state) {
+      return state.email;
+    },
+    userId(state) {
+      return state.userId;
+    },
+    firstName(state) {
+      return state.firstName;
     },
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    setUser(state, payload) {
+      state.token = payload.token;
+      state.userId = payload.userId;
+      state.email = payload.email;
+      state.firstName = payload.firstName;
+    },
+    resetUser(state) {
+      state.token = null;
+      state.userId = null;
+      state.email = null;
+      state.firstName = null;
+    },
+  },
+  actions: {
+    login(context, payload) {
+      context.commit("setUser", payload);
+    },
+    logout(context, payload) {
+      context.commit("resetUser", payload);
+    },
+  },
   modules: {},
 });
